914151b5338efd6229a4afbecbdf19691d42e917
Carlos <U202015274@upc.edu.pe>
2026-01-20 19:27:08 -0500

    feat: implement AuthenticationController with sign-in and sign-up endpoints

---
+++ IAM/Interfaces/REST/AuthenticationController.cs
@@ -0,0 +1,47 @@
+using System.Net.Mime;
+using dermakardex_backend.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using IAM.Domain.Services;
+using IAM.Intefaces.REST.Resources;
+using IAM.Interfaces.REST.Resources;
+using IAM.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace IAM.Interfaces.REST;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Authentication endpoints")]
+public class AuthenticationController(IUserCommandService userCommandService) : ControllerBase
+{
+    [HttpPost("sign-in")]
+    [AllowAnonymous]
+    [SwaggerOperation(
+        Summary = "Sign in",
+        Description = "Sign in a user",
+        OperationId = "SignIn")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The user was authenticated", typeof(AuthenticatedUserResource))]
+    public async Task<IActionResult> SignIn([FromBody] SignInResource signInResource)
+    {
+        var signInCommand = SignInCommandFromResourceAssembler.ToCommandFromResource(signInResource);
+        var authenticateduser = await userCommandService.Handle(signInCommand);
+        var resource = AuthenticatedUserResourceFromEntityAssembler.ToResourceFromEntity(authenticateduser.user, authenticateduser.token);
+        return Ok(resource);
+    }
+
+    [HttpPost("sign-up")]
+    [AllowAnonymous]
+    [SwaggerOperation(
+            Summary = "Sign-up",
+            Description = "Sign up a new user",
+            OperationId = "SignUp")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The user was created successfully")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpResource signUpResource)
+    {
+        var signUpCommand = SignUpCommandFromResourceAssembler.ToCommandFromResource(signUpResource);
+        await userCommandService.Handle(signUpCommand);
+        return Ok(new { message = "User created successfully" });
+    }
+}
