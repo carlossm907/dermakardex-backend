7e5b10b2b229ac34212b5438211dca0a709ab973
Carlos <U202015274@upc.edu.pe>
2026-01-20 19:27:25 -0500

    feat: add IAM configuration application in AppDbContext

--- Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using IAM.Infrastructure.Persistence.EF.Configuration.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -14,6 +15,8 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.ApplyIamConfiguration();
         
         // Use snake case for database objects and pluralization for table names
         builder.UseSnakeCaseNamingConvention();
