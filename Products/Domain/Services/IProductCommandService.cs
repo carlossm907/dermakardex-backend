9e5d421be716b6986b0744c7d9287934bf8d8b66
Carlos <U202015274@upc.edu.pe>
2026-01-22 15:43:05 -0500

    feat(products): add command and query services for products

---
+++ Products/Domain/Services/IProductCommandService.cs
@@ -0,0 +1,21 @@
+using Products.Domain.Model.Aggregates;
+using Products.Domain.Model.Commands;
+
+namespace Products.Domain.Services;
+
+public interface IProductCommandService
+{
+    Task<Product?> Handle(CreateProductCommand command);
+
+    Task<Product?> Handle(UpdateProductCommand command);
+
+    Task Handle(ChangeProductPricesCommand command);
+
+    Task Handle(ChangeProductMaxDiscountCommand command);
+
+    Task Handle(AdjustProductStockCommand command);
+
+    Task Handle(ActivateProductCommand command);
+
+    Task Handle(DeactivateProductCommand command);
+}
---
+++ Products/Domain/Services/IProductQueryService.cs
@@ -0,0 +1,14 @@
+using Products.Domain.Model.Aggregates;
+using Products.Domain.Model.Queries;
+
+namespace Products.Domain.Services;
+
+
+public interface IProductQueryService
+{
+    Task<Product?> Handle(GetProductByIdQuery query);
+
+    Task<IEnumerable<Product>> Handle(ListProductsQuery query);
+
+    Task<IEnumerable<Product>> Handle(GetLowStockProductsQuery query);
+}
