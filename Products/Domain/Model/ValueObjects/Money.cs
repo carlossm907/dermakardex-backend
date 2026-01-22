68411ef6be26985ef50016744e7d791806788d6a
Carlos <U202015274@upc.edu.pe>
2026-01-21 23:08:44 -0500

    feat(money): add Money value object to represent monetary amounts with currency

---
+++ Products/Domain/Model/ValueObjects/Money.cs
@@ -0,0 +1,16 @@
+namespace Products.Domain.Model.Aggregates;
+
+public record Money
+{
+    public decimal Amount { get; }
+    public string Currency { get; }
+
+    public Money(decimal amount, string currency = "PEN")
+    {
+        if (amount < 0)
+            throw new ArgumentException("Amount cannot be negative");
+
+        Amount = decimal.Round(amount, 2);
+        Currency = currency;
+    }
+}
