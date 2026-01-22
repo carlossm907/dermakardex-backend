2f520e9a43e49d7a67eb9b572ba5a19f50b05d62
Carlos <U202015274@upc.edu.pe>
2026-01-22 14:58:28 -0500

    feat(products): add commands for creating, deleting, and updating categories

---
+++ Products/Domain/Model/Commands/DeleteCategoryCommand.cs
@@ -0,0 +1,3 @@
+namespace Products.Domain.Model.Commands;
+
+public record DeleteCategoryCommand(int CategoryId);
---
+++ Products/Domain/Model/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,3 @@
+namespace Products.Domain.Model.Commands;
+
+public record UpdateCategoryCommand(int CategoryId, string Name);
