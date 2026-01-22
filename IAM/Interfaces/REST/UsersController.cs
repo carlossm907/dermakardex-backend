35942dc5d662f9ef307b931ac3bd589d5284df23
Carlos <U202015274@upc.edu.pe>
2026-01-21 20:22:33 -0500

    feat: implement UsersController with endpoints to get user by id and retrieve all users

---
+++ IAM/Interfaces/REST/UsersController.cs
@@ -0,0 +1,47 @@
+using System.Net.Mime;
+using dermakardex_backend.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using IAM.Domain.Model.Queries;
+using IAM.Domain.Services;
+using IAM.Interfaces.REST.Resources;
+using IAM.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace IAM.Interfaces.REST;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available User endpoints")]
+public class UsersController(IUserQueryService userQueryService) : ControllerBase
+{
+
+    [HttpGet("{id}")]
+    [SwaggerOperation(
+        Summary = "Get a user by its id",
+        Description = "Get a user by its id",
+        OperationId = "GetUserById")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(id);
+        var user = await userQueryService.Handle(getUserByIdQuery);
+        var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user!);
+        return Ok(userResource);
+    }
+
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all users",
+        Description = "Get all users",
+        OperationId = "GetAllUsers")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The users were found", typeof(IEnumerable<UserResource>))]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var getAllUsersQuery = new GetAllUsersQuery();
+        var users = await userQueryService.Handle(getAllUsersQuery);
+        var userResources = users.Select(UserResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(userResources);
+    }
+    
+}
