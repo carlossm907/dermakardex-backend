abc1336967d0e3ecb2a47284ec822375dd9a5c55
Carlos <U202015274@upc.edu.pe>
2026-01-23 10:51:48 -0500

    feat(resources): add CreateLaboratoryResource, LaboratoryResource, and UpdateLaboratoryResource records

---
+++ Products/Interfaces/REST/Resources/LaboratoryResource.cs
@@ -0,0 +1,6 @@
+namespace Products.Interfaces.REST.Resources;
+
+public record LaboratoryResource(
+    int Id,
+    string Name
+);
---
+++ Products/Interfaces/REST/Resources/UpdateLaboratoryResource.cs
@@ -0,0 +1,5 @@
+namespace Products.Interfaces.REST.Resources;
+
+public record UpdateLaboratoryResource(
+    string Name
+);
