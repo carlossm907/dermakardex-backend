f50fba8707e821ec11cacb47d7a645f295f91ea9
Carlos <U202015274@upc.edu.pe>
2026-01-23 09:49:30 -0500

    feat(services): add command and query services for Product to WebApplicationBuilderExtensions

--- Products/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
+++ Products/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,5 +1,8 @@
+using Products.Application.Internal.CommandServices;
+using Products.Application.Internal.QueryServices;
 using Products.Domain.Repositoies;
 using Products.Domain.Repositories;
+using Products.Domain.Services;
 using Products.Infrastructure.Persistence.EFC.Repositories;
 
 namespace Products.Infrastructure.Interfaces.ASP.Configuration.Extensions;
@@ -13,6 +16,8 @@ public static class WebApplicationBuilderExtensions
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<ILaboratoryRepository, LaboratoryRepository>();
         services.AddScoped<ISupplierRepository, SupplierRepository>();
+        services.AddScoped<IProductCommandService, ProductCommandService>();
+        services.AddScoped<IProductQueryService, ProductQueryService>();
 
         return services;
     }
