d51cc255cd873a7cfc01d137ad501088c087dbb1
Carlos <U202015274@upc.edu.pe>
2026-01-22 21:39:53 -0500

    feat(repository): implement ProductRepository with FindLowStockAsync and ListAsync methods

---
+++ Products/Infrastructure/Persistence/EFC/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+using dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Products.Domain.Model.Aggregates;
+using Products.Domain.Repositories;
+
+namespace Products.Infrastructure.Persistence.EFC.Repositories;
+
+public class ProductRepository(AppDbContext context) : BaseRepository<Product>(context), IProductRepository
+{
+    public async Task<IEnumerable<Product>> FindLowStockAsync()
+
+        => await Context.Set<Product>()
+            .Where(p => p.Stock <= p.StockAlertThreshold)
+            .ToListAsync();
+
+    public async Task<IEnumerable<Product>> ListAsync(string? Name)
+    {
+        var query = Context.Set<Product>().AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(Name))
+            query = query.Where(p => p.Name.Contains(Name));
+
+        return await query.ToListAsync();
+    }
+}
