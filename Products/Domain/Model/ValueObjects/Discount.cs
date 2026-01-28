d221b4d71cb4861ebd7b09e7c262c705366796cb
Carlos <U202015274@upc.edu.pe>
2026-01-27 20:09:48 -0500

    feat(value-objects): implement Discount value object for discount management

---
+++ Products/Domain/Model/ValueObjects/Discount.cs
@@ -0,0 +1,59 @@
+using Products.Domain.Model.Aggregates;
+
+namespace Products.Domain.Model.ValueObjects;
+
+public class Discount
+{
+    public DiscountType Type { get; }
+    public decimal Value { get; }
+
+    protected Discount() { }
+
+    private Discount(DiscountType type, decimal value)
+    {
+        Type = type;
+        Value = value;
+        Validate();
+    }
+
+    public static Discount None()
+    {
+        return new(DiscountType.None, 0);
+    }
+
+    public static Discount Amount(decimal amount)
+    {
+        return new(DiscountType.Amount, amount);
+    }
+
+    public static Discount Percentaje(decimal percentage)
+    {
+        return new(DiscountType.Percentaje, percentage);
+    }
+
+    private void Validate()
+    {
+        if (Value < 0)
+        {
+            throw new ArgumentException("Discount canot be less than 0");
+        }
+
+        if (Type == DiscountType.Percentaje && Value > 100)
+        {
+            throw new ArgumentException("Percentage discount cannot exceed 100%");
+        }
+    }
+
+    public Money CalculateDiscount(Money salePrice)
+    {
+        switch (Type)
+        {
+            case DiscountType.Amount:
+                return new Money(Value);
+            case DiscountType.Percentaje:
+                return new Money(salePrice.Amount * (Value / 100m));
+            default:
+                return new Money(0);
+        }
+    }
+}
