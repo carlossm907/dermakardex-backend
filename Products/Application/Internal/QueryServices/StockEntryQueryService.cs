5ebddfc1208110da92dbb364cf4d5267933bfc2c
Carlos <U202015274@upc.edu.pe>
2026-01-27 19:33:00 -0500

    feat(services): add StockEntryQueryService for querying stock entries

---
+++ Products/Application/Internal/QueryServices/StockEntryQueryService.cs
@@ -0,0 +1,19 @@
+using Products.Domain.Model.Entities;
+using Products.Domain.Model.Queries;
+using Products.Domain.Repositories;
+using Products.Domain.Services;
+
+namespace Products.Application.Internal.QueryServices;
+
+public class StockEntryQueryService(IStockEntryRepository stockEntryRepository) : IStockEntryQueryService
+{
+    public async Task<IEnumerable<StockEntry>> Handle(GetProductStockEntriesQuery query)
+    {
+        return await stockEntryRepository.FindByProductIdAsync(query.ProductId);
+    }
+
+    public async Task<IEnumerable<StockEntry>> Handle(GetAllStockEntriesQuery query)
+    {
+        return await stockEntryRepository.FindAllAsync();
+    }
+}
