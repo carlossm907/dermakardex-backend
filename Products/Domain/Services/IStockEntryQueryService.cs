4e4a0b1706ba56cedd279d9a16b927e536b6988d
Carlos <U202015274@upc.edu.pe>
2026-01-27 19:29:20 -0500

    feat(services): add IStockEntryQueryService interface with query handling methods

---
+++ Products/Domain/Services/IStockEntryQueryService.cs
@@ -0,0 +1,11 @@
+using Products.Domain.Model.Entities;
+using Products.Domain.Model.Queries;
+
+namespace Products.Domain.Services;
+
+public interface IStockEntryQueryService
+{
+    Task<IEnumerable<StockEntry>> Handle(GetProductStockEntriesQuery query);
+
+    Task<IEnumerable<StockEntry>> Handle(GetAllStockEntriesQuery query);
+}
