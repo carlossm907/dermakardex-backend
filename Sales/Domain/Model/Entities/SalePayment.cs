d8fae1073d60069731f5202330af8526fbccfdbc
Carlos <U202015274@upc.edu.pe>
2026-01-29 14:25:33 -0500

    feat(entities): add SalePayment entity with properties and constructor

---
+++ Sales/Domain/Model/Entities/SalePayment.cs
@@ -0,0 +1,31 @@
+using Products.Domain.Model.Aggregates;
+using Sales.Domain.Model.ValueObjects;
+
+namespace Sales.Domain.Model.Entities;
+
+public class SalePayment
+{
+    public int Id { get; private set; }
+    public PaymentMethod Method { get; private set; }
+    public Money Amount { get; private set; }
+
+    protected SalePayment()
+    {
+        Amount = new Money(0);
+    }
+
+    public SalePayment(
+        PaymentMethod method,
+        Money amount
+    )
+    {
+        if (amount.Amount <= 0)
+        {
+            throw new ArgumentException("Payment must be greater than zero");
+        }
+
+        Method = method;
+        Amount = amount;
+    }
+
+}
