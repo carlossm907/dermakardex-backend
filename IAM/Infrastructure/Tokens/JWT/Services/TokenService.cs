10225f0862b7591c8ec5fc63476a083325c717ea
Carlos <U202015274@upc.edu.pe>
2026-01-29 23:48:18 -0500

    feat(services): update token claims to use fullname instead of username

--- IAM/Infrastructure/Tokens/JWT/Services/TokenService.cs
+++ IAM/Infrastructure/Tokens/JWT/Services/TokenService.cs
@@ -23,7 +23,7 @@ public class TokenService(IOptions<TokenSettings> tokenSettings) : ITokenService
             Subject = new ClaimsIdentity(new[]
             {
                 new Claim(ClaimTypes.Sid, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Username.ToString()),
+                new Claim("fullname", user.FullName.Value),
                 new Claim(ClaimTypes.Role, user.Role.GetStringName())
             }),
             Expires = DateTime.UtcNow.AddDays(7),
