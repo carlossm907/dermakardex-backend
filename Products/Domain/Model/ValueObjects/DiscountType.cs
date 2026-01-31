3a3c6aeb5bc248d3a13bac8da9e281b9b06efc6b
Carlos <U202015274@upc.edu.pe>
2026-01-31 00:14:48 -0500

    refactor(value-objects): remove unused DiscountType, Money, and ProductPresentation value objects

--- Products/Domain/Model/ValueObjects/DiscountType.cs
+++
@@ -1,8 +0,0 @@
-namespace Products.Domain.Model.ValueObjects;
-
-public enum DiscountType
-{
-    None = 0,
-    Amount = 1,
-    Percentaje = 2
-}
--- Products/Domain/Model/ValueObjects/ProductPresentation.cs
+++
@@ -1,8 +0,0 @@
-namespace Products.Domain.Model.Aggregates;
-
-public enum ProductPresentation
-{
-    Units = 1,
-    HalfDozen = 6,
-    Dozen = 12
-}
