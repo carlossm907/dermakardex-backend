1ef433ada1e004547f3618ab5994c55eb0ef409a
Carlos <U202015274@upc.edu.pe>
2026-03-05 08:21:21 -0500

    feat(WebApplicationExtensions): replace EnsureCreated with Migrate for database initialization

--- Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/WebApplicationExtensions.cs
+++ Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/WebApplicationExtensions.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 public static class WebApplicationExtensions
@@ -6,6 +8,6 @@ public static class WebApplicationExtensions
     {
         using var scope = app.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        context.Database.EnsureCreated();
+        context.Database.Migrate();
     }
 }
