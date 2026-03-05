4fd90ebdd4fefe468cc385234ed2113b3be981bb
Carlos <U202015274@upc.edu.pe>
2026-03-05 08:35:13 -0500

    feat(AddUserFullNameToProductStockEntries): remove registered_by_user_id and add user_full_name to product_stock_entries

---
+++ Migrations/20260301165756_AddUserFullNameToProductStockEntries.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dermakardex_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFullNameToProductStockEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "registered_by_user_id",
+                table: "product_stock_entries");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "registered_at",
+                table: "product_stock_entries",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "user_full_name",
+                table: "product_stock_entries",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "user_full_name",
+                table: "product_stock_entries");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "registered_at",
+                table: "product_stock_entries",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AddColumn<int>(
+                name: "registered_by_user_id",
+                table: "product_stock_entries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
