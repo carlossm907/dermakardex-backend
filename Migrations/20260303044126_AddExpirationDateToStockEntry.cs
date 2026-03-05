ffe70fce64894c5b5aa208c30c857e38cfd3648d
Carlos <U202015274@upc.edu.pe>
2026-03-05 08:35:27 -0500

    feat(AddExpirationDateToStockEntry): add expiration_date column to product_stock_entries

---
+++ Migrations/20260303044126_AddExpirationDateToStockEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dermakardex_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpirationDateToStockEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "expiration_date",
+                table: "product_stock_entries",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expiration_date",
+                table: "product_stock_entries");
+        }
+    }
+}
