0ac58e9d7a21e89d7bba68e7c29613ef42d8534d
Carlos <U202015274@upc.edu.pe>
2026-03-05 08:35:43 -0500

    feat(AddCodeToProduct): add code column to products table

---
+++ Migrations/20260304192009_AddCodeToProduct.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dermakardex_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "code",
+                table: "products",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "code",
+                table: "products");
+        }
+    }
+}
