548bedc6e2d8558fa6d258ca1c0f8742d6c44066
Carlos <U202015274@upc.edu.pe>
2026-03-09 23:06:11 -0500

    feat(ProductDiscount): add Name column to product_discounts table and update model snapshot

---
+++ Migrations/20260310032657_AddNameToProductDiscounts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dermakardex_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToProductDiscounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "product_discounts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "product_discounts");
+        }
+    }
+}
