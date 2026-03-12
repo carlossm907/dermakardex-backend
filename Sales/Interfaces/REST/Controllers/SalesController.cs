5812cfa06efd35968002408c2e5e6aa100b559dd
Carlos <U202015274@upc.edu.pe>
2026-03-11 22:57:11 -0500

    feat(SalesController): add endpoints for sales report by day and month

--- Sales/Interfaces/REST/Controllers/SalesController.cs
+++ Sales/Interfaces/REST/Controllers/SalesController.cs
@@ -3,6 +3,7 @@ using Sales.Domain.Model.Queries;
 using Sales.Domain.Services;
 using Sales.Interfaces.REST.Resources;
 using Sales.Interfaces.REST.Transform;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Sales.Interfaces.REST.Controllers;
 
@@ -80,6 +81,44 @@ public class SalesController(ISaleCommandService saleCommandService, ISaleQueryS
         var sales = await saleQueryService.Handle(query);
 
         var resources = sales.Select(SaleResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(resources);
+    }
+
+    [HttpGet("report/day/{date}")]
+    [SwaggerOperation(
+    Summary = "Get Sales Report By Day",
+    Description = "Returns all sales grouped by customer for a specific day.",
+    OperationId = "GetSalesReportByDay")]
+    [SwaggerResponse(200, "Sales report retrieved successfully.",
+    typeof(IEnumerable<SalesGroupedByCustomerReportResource>))]
+    public async Task<IActionResult> GetSalesReportByDay(DateOnly date)
+    {
+        var query = new GetSalesGroupedByCustomerByDayQuery(date);
+
+        var report = await saleQueryService.Handle(query);
+
+        var resources = report
+            .Select(SalesGroupedByCustomerReportResourceFromModelAssembler.ToResourceFromModel);
+
+        return Ok(resources);
+    }
+
+    [HttpGet("report/month/{year:int}/{month:int}")]
+    [SwaggerOperation(
+    Summary = "Get Sales Report By Month",
+    Description = "Returns all sales grouped by customer for a specific month.",
+    OperationId = "GetSalesReportByMonth")]
+    [SwaggerResponse(200, "Sales report retrieved successfully.",
+    typeof(IEnumerable<SalesGroupedByCustomerReportResource>))]
+    public async Task<IActionResult> GetSalesReportByMonth(int year, int month)
+    {
+        var query = new GetSalesGroupedByCustomerByMonthQuery(year, month);
+
+        var report = await saleQueryService.Handle(query);
+
+        var resources = report
+            .Select(SalesGroupedByCustomerReportResourceFromModelAssembler.ToResourceFromModel);
+
         return Ok(resources);
     }
 
