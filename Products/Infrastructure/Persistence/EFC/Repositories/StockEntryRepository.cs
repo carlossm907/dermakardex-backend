5c9eccdc178d3bb1868c11b955be45df1d55e4ae
Carlos <U202015274@upc.edu.pe>
2026-03-15 21:06:53 -0500

    feat(StockEntryRepository): implement methods to retrieve product entries per day

--- Products/Infrastructure/Persistence/EFC/Repositories/StockEntryRepository.cs
+++ Products/Infrastructure/Persistence/EFC/Repositories/StockEntryRepository.cs
@@ -2,6 +2,7 @@ using dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
 using dermakardex_backend.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Products.Domain.Model.Entities;
+using Products.Domain.Model.ReadModels;
 using Products.Domain.Repositories;
 
 namespace Products.Infrastructure.Persistence.EFC.Repositories;
@@ -21,5 +22,48 @@ public class StockEntryRepository(AppDbContext context) : BaseRepository<StockEn
                 .Where(e => e.ProductId == productId)
                 .OrderByDescending(e => e.RegisteredAt)
                 .ToListAsync();
+    }
+
+    public async Task<IEnumerable<ProductEntriesPerDay>> FindProductEntriesPerDayAsync(int productId, DateOnly from, DateOnly to)
+    {
+        return await Context.Set<StockEntry>()
+        .Where(e =>
+            e.ProductId == productId &&
+            DateOnly.FromDateTime(e.RegisteredAt) >= from &&
+            DateOnly.FromDateTime(e.RegisteredAt) <= to
+        )
+        .GroupBy(e => new
+        {
+            e.ProductId,
+            Date = DateOnly.FromDateTime(e.RegisteredAt)
+        })
+        .Select(g => new ProductEntriesPerDay
+        {
+            ProductId = g.Key.ProductId,
+            Date = g.Key.Date,
+            Quantity = g.Sum(e => e.Quantity)
+        })
+        .ToListAsync();
+    }
+
+    public async Task<IEnumerable<ProductEntriesPerDay>> FindProductsEntriesPerDayAsync(DateOnly from, DateOnly to)
+    {
+        return await Context.Set<StockEntry>()
+        .Where(e =>
+            DateOnly.FromDateTime(e.RegisteredAt) >= from &&
+            DateOnly.FromDateTime(e.RegisteredAt) <= to
+        )
+        .GroupBy(e => new
+        {
+            e.ProductId,
+            Date = DateOnly.FromDateTime(e.RegisteredAt)
+        })
+        .Select(g => new ProductEntriesPerDay
+        {
+            ProductId = g.Key.ProductId,
+            Date = g.Key.Date,
+            Quantity = g.Sum(x => x.Quantity)
+        })
+        .ToListAsync();
     }
 }
