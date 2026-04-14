f4ea34d4f722367015628c018b54a685feeb73c5
Carlos <U202015274@upc.edu.pe>
2026-04-14 16:54:36 -0500

    feat(IProductDiscountQueryService): update namespace for product discount queries

--- Products/Domain/Services/QueryServices/IProductDiscountQueryService.cs
+++ Products/Domain/Services/QueryServices/IProductDiscountQueryService.cs
@@ -1,6 +1,7 @@
 using Products.Domain.Model.Aggregates;
 using Products.Domain.Model.Entities;
-using Products.Domain.Model.Queries;
+using Products.Domain.Model.Queries.ScheduledDiscount;
+
 
 namespace Products.Domain.Services;
 
