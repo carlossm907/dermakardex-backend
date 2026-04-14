4bd5e400abe0be615ec1682e7c84a13a2e2052b5
Carlos <U202015274@upc.edu.pe>
2026-04-14 18:10:33 -0500

    refactor(WebApplicationBuilderExtensions): reorganize service registrations and add missing repositories

--- Products/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
+++ Products/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Products.Application.Internal.CommandServices;
 using Products.Application.Internal.QueryServices;
 using Products.Domain.Repositories;
 using Products.Domain.Services;
+using Products.Domain.Services.QueryServices;
 using Products.Infrastructure.Persistence.EFC.Repositories;
 using Products.Interfaces.ACL;
 
@@ -12,27 +13,38 @@ public static class WebApplicationBuilderExtensions
 {
     public static IServiceCollection AddProductsContextServices(this IServiceCollection services)
     {
+        // Query Services
+        services.AddScoped<IProductQueryService, ProductQueryService>();
+        services.AddScoped<IBrandQueryService, BrandQueryService>();
+        services.AddScoped<ICategoryQueryService, CategoryQueryService>();
+        services.AddScoped<ILaboratoryQueryService, LaboratoryQueryService>();
+        services.AddScoped<ISupplierQueryService, SupplierQueryService>();
+        services.AddScoped<IStockEntryQueryService, StockEntryQueryService>();
+        services.AddScoped<IScheduledDiscountQueryService, ScheduledDiscountQueryService>();
+        services.AddScoped<ISalesProductReportQueryService, SalesProductReportQueryService>();
+        services.AddScoped<IStockEntryProductReportQueryService, StockEntryProductReportQueryService>();
+        services.AddScoped<IStockReportQueryService, StockReportQueryService>();
+
+        // Command Services
+        services.AddScoped<IProductCommandService, ProductCommandService>();
+        services.AddScoped<IBrandCommandService, BrandCommandService>();
+        services.AddScoped<ICategoryCommandService, CategoryCommandService>();
+        services.AddScoped<ILaboratoryCommandService, LaboratoryCommandService>();
+        services.AddScoped<ISupplierCommandService, SupplierCommandService>();
+        services.AddScoped<IDiscountsCommandService, DiscountsCommandService>();
+        services.AddScoped<IScheduledDiscountCommandService, ScheduledDiscountCommandService>();
+
+        // Repositories
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IBrandRepository, BrandRepository>();
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<ILaboratoryRepository, LaboratoryRepository>();
         services.AddScoped<ISupplierRepository, SupplierRepository>();
         services.AddScoped<IStockEntryRepository, StockEntryRepository>();
-        services.AddScoped<IProductCommandService, ProductCommandService>();
-        services.AddScoped<IProductQueryService, ProductQueryService>();
-        services.AddScoped<IBrandCommandService, BrandCommandService>();
-        services.AddScoped<IBrandQueryService, BrandQueryService>();
-        services.AddScoped<ICategoryCommandService, CategoryCommandService>();
-        services.AddScoped<ICategoryQueryService, CategoryQueryService>();
-        services.AddScoped<ILaboratoryCommandService, LaboratoryCommandService>();
-        services.AddScoped<ILaboratoryQueryService, LaboratoryQueryService>();
-        services.AddScoped<ISupplierCommandService, SupplierCommandService>();
-        services.AddScoped<ISupplierQueryService, SupplierQueryService>();
-        services.AddScoped<IStockEntryQueryService, StockEntryQueryService>();
-        services.AddScoped<IProductsContextFacade, ProductsContextFacade>();
-        services.AddScoped<IScheduledDiscountCommandService, ScheduledDiscountCommandService>();
-        services.AddScoped<IProductDiscountQueryService, ProductDiscountQueryService>();
         services.AddScoped<IProductDiscountRepository, ProductDiscountRepository>();
+
+        // ACL Facade
+        services.AddScoped<IProductsContextFacade, ProductsContextFacade>();
 
         return services;
     }
