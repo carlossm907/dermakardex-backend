c9843c6eb7c8e1e9f759452c752ddac4d63bf0da
Carlos <U202015274@upc.edu.pe>
2026-04-14 16:47:17 -0500

    feat(SalesProductReportQueries): add ISalesProductReportQueryService interface for sales report queries

--- Products/Domain/Services/QueryServices/IProductQueryService.cs
+++ Products/Domain/Services/QueryServices/IProductQueryService.cs
@@ -25,14 +25,6 @@ public interface IProductQueryService
 
     Task<IEnumerable<ProductDailyStockReportItem>> Handle(GetAffectedProductsDailyStockReportQuery query);
 
-    Task<IEnumerable<ProductSalesReportItem>> Handle(GetProductSalesReportQuery query);
-
-    Task<IEnumerable<ProductSalesReportItem>> Handle(GetProductsSalesReportQuery query);
-
-    Task<IEnumerable<ProductSalesReportItem>> Handle(GetAllProductsSalesReportQuery query);
-
-    Task<IEnumerable<ProductSalesReportItem>> Handle(GetAffectedProductsSalesReportQuery query);
-
     Task<IEnumerable<ProductDailyEntriesReportItem>> Handle(GetProductEntriesReportQuery query);
 
     Task<IEnumerable<ProductDailyEntriesReportItem>> Handle(GetProductsEntriesReportQuery query);
---
+++ Products/Domain/Services/QueryServices/ISalesProductReportQueryService.cs
@@ -0,0 +1,15 @@
+using Products.Domain.Model.Queries.SalesProductReport;
+using Products.Domain.Model.ReadModels;
+
+namespace Products.Domain.Services.QueryServices;
+
+public interface ISalesProductReportQueryService
+{
+    Task<IEnumerable<ProductSalesReportItem>> Handle(GetProductSalesReportQuery query);
+
+    Task<IEnumerable<ProductSalesReportItem>> Handle(GetProductsSalesReportQuery query);
+
+    Task<IEnumerable<ProductSalesReportItem>> Handle(GetAllProductsSalesReportQuery query);
+
+    Task<IEnumerable<ProductSalesReportItem>> Handle(GetAffectedProductsSalesReportQuery query);
+}
