106fef327452e6c7ece15876eab3805ad80b3f4e
Carlos <U202015274@upc.edu.pe>
2026-04-14 17:33:21 -0500

    fix(SupplierQueryService): update import statement for supplier queries

--- Products/Application/Internal/QueryServices/SupplierQueryService.cs
+++ Products/Application/Internal/QueryServices/SupplierQueryService.cs
@@ -1,5 +1,5 @@
 using Products.Domain.Model.Entities;
-using Products.Domain.Model.Queries;
+using Products.Domain.Model.Queries.Supplier;
 using Products.Domain.Repositories;
 using Products.Domain.Services;
 
