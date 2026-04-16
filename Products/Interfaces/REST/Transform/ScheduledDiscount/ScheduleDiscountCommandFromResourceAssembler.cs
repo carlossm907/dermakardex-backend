70dd0538c42d0c51a215a182fee82073c566a6fc
Carlos <U202015274@upc.edu.pe>
2026-04-15 22:36:16 -0500

    feat(ScheduleDiscountCommandFromResourceAssembler): update ToCommandFromResource method to accept productId as a parameter

--- Products/Interfaces/REST/Transform/ScheduledDiscount/ScheduleDiscountCommandFromResourceAssembler.cs
+++ Products/Interfaces/REST/Transform/ScheduledDiscount/ScheduleDiscountCommandFromResourceAssembler.cs
@@ -5,10 +5,10 @@ namespace Products.Interfaces.REST.Transform;
 
 public static class ScheduleDiscountCommandFromResourceAssembler
 {
-    public static ScheduleDiscountToProductCommand ToCommandFromResource(ScheduleDiscountResource resource)
+    public static ScheduleDiscountToProductCommand ToCommandFromResource(int productId, ScheduleDiscountResource resource)
     {
         return new ScheduleDiscountToProductCommand(
-            resource.ProductId,
+            productId,
             resource.Name,
             resource.Type,
             resource.Value,
