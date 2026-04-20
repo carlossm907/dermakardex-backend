6326fd8162f69080a20f84d4d04896ded7b6f6ed
Carlos <U202015274@upc.edu.pe>
2026-04-19 23:01:31 -0500

    feat(WebApplicationBuilderExtensions): organize and add missing services in AddSalesContextServices method

--- Sales/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
+++ Sales/Infrastructure/Interfaces/ASP/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,9 +1,11 @@
 using Sales.Application.ACL;
 using Sales.Application.Internal.CommandServices;
 using Sales.Application.Internal.QueryServices;
+using Sales.Application.Internal.OutboundServices;
 using Sales.Domain.Repositories;
 using Sales.Domain.Services;
 using Sales.Infrastructure.Persistence.EFC.Repositories;
+using Sales.Infrastructure.ExternalServices.Dni;
 using Sales.Interfaces.ACL;
 
 namespace Sales.Infrastructure.Interfaces.ASP.Configuration.Extensions;
@@ -12,9 +14,20 @@ public static class WebApplicationBuilderExtensions
 {
     public static void AddSalesContextServices(this WebApplicationBuilder builder)
     {
+        // Command Services
         builder.Services.AddScoped<ISaleCommandService, SaleCommandService>();
+
+        // Query Services
         builder.Services.AddScoped<ISaleQueryService, SaleQueryService>();
+        builder.Services.AddScoped<ISaleByClientQueryService, SaleByClientQueryService>();
+        builder.Services.AddScoped<ISaleBySellerQueryService, SaleBySellerQueryService>();
+        builder.Services.AddScoped<ISaleFilterQueryService, SaleFilterQueryService>();
+        builder.Services.AddScoped<ISaleForStockReportQueryService, SaleForStockReportQueryService>();
+
+        // Repositories
         builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+
+        // Facades
         builder.Services.AddScoped<ISalesContextFacade, SalesContextFacade>();
     }
 }
